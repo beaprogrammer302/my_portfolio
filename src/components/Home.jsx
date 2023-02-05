04f57691b71939b9b942938e212a1bfbd715a1c5
Rashid Pervaiz <rashidpervaiz302@gmail.com>
2023-02-05 23:19:44 +0500

    'change'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800 pt-20'>
        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
          <div className='flex flex-col justify-center h-full'>
-            <h2 className='text-4xl sm:text-7xl font-bold text-white '>I'm a Front-End Developer</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-white mb-6'>I'm a Front-End Developer</h2>
             <p className='text-white my-4'>
             Hi my name is Rashid Pervaiz. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the 
             visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used
