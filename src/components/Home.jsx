db6387d3ac87814ad89815a37d77344970e4f7af
Rashid Pervaiz <rashidpervaiz302@gmail.com>
2023-02-06 00:43:52 +0500

    'cmt'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
                 </button>
             </div>
          </div>
-         <div className='w-2/3 h-96 pb-16 '>
+         <div className='w-2/3 h-96 pb-60 '>
             <img src='rashid2.jpg' alt='my_profile' className='rounded-2xl mx-auto w-screen sm:h-2 md:w-full h-72'/>
          </div>
        </div>
